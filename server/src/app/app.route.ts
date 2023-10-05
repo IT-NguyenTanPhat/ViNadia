af7a558515ec0eecb0b83b5d720f6df98599ae9a
Nguyen Tan Phat <tanphat200265@gmail.com>
2023-10-05 21:29:10 +0700

    fix deploy server

--- server/src/app/app.route.ts
+++ server/src/app/app.route.ts
@@ -8,7 +8,7 @@ import definition from '../docs/swagger';
 
 const router = Router();
 
-router.get('/', (req, res) => res.redirect('api-docs'));
+router.get('/', (req, res) => res.redirect('/api-docs'));
 
 router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(definition));
 
